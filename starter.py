7516da6b279ea0ad1da58afffbad5fbdeac79fc9
Prakhar Srivastav <prakhar1989@gmail.com>
2015-09-18 17:37:12 -0400

    classes and more classes

--- starter.py
+++ starter.py
@@ -3,34 +3,41 @@
 import base64
 import requests
 
+# Constants
 KEY = "Byygq1zI2KKyssKp8UvVe3DV/v6Aa0FEsKrE+pqDa0s"
 ID = "0c3454d3-67ce-4558-b4ac-1e95f964cdf5"
+ENCODED = base64.b64encode("{0}:{1}".format(KEY, KEY))
 
-encoded = base64.b64encode("{0}:{1}".format(KEY, KEY))
-headers = {'Authorization': 'Basic ' + encoded}
-
-def getURIFromQuery(query):
-    url = "https://api.datamarket.azure.com/Bing/Search/Web?Query=%27{0}%27&$top=10&$format=json"
-    return url.format(query)
+class BingBing():
+    def __init__(self, query, precision):
+        self.query = query
+        self.precision = precision
 
 
-def getResults(query):
-    url = getURIFromQuery(query)
-    r = requests.get(url, headers=headers)
-    return r.json()
+    def getURI(self):
+        url = "https://api.datamarket.azure.com/Bing/Search/Web?Query=%27{0}%27&$top=10&$format=json"
+        return url.format(self.query)
 
-def printResults(results):
-    print "-"*50
-    for i, r in enumerate(results):
-        print i, r["Title"], "(" + r["Url"] + ")"
-        print " ", r["Description"]
+    def getResults(self):
+        url = self.getURI()
+        headers = {'Authorization': 'Basic ' + ENCODED}
+        r = requests.get(url, headers=headers)
+        data = r.json()
+        return data.get('d').get('results')
+
+    def printResults(self, results):
         print "-"*50
+        for i, r in enumerate(results):
+            print i, r["Title"], "(" + r["Url"] + ")"
+            print " ", r["Description"]
+            print "-"*50
 
-def begin(query):
-    results = getResults(query)["d"]["results"]
-    printResults(results)
-    print "Enter which all results are relevant (comma separated):",
-    relevant_indices = raw_input()
+    def start(self):
+        results = self.getResults()
+        self.printResults(results)
+        print "Enter which all results are relevant (comma separated):",
+        relevant_indices = raw_input()
 
 if __name__ == "__main__":
-    begin("gates")
+    b = BingBing(query="gates", precision=0.5)
+    b.start()
