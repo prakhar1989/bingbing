998bf44a378ce33fac4eb52403d912a786de6e71
hellking4u <hellking4u@gmail.com>
2015-09-22 16:41:16 -0400

    PEP8 cleanup

--- starter.py
+++ starter.py
@@ -1,51 +1,79 @@
-#!/usr/bin/python
+"""
+API function implementation
+"""
+
+# !/usr/bin/python
 
 import requests
 
 import dev_config as config
 # Constants
 
+
 class BingBing():
+    """
+    main entry class
+    """
+
     def __init__(self, query, precision):
+        """
+        :param query: the query string
+        :param precision: the requisite accuracy.
+        """
         self.query = query
         self.precision = precision
         self.selectedIDs = []
         self.results = []
 
-    def getURI(self):
+    def get_uri(self):
+        """
+        :return: the WebQuery URI.
+        """
         url = "https://api.datamarket.azure.com/Bing/Search/Web?Query=%27{0}%27&$top=10&$format=json"
         return url.format(self.query)
 
-    def getResults(self):
-        url = self.getURI()
+    def get_results(self):
+        """
+
+        :return: [requests object] get the results.
+        """
+        url = self.get_uri()
         headers = {'Authorization': 'Basic ' + config.ENCODED_KEY}
         r = requests.get(url, headers=headers)
         data = r.json()
         return data.get('d').get('results')
 
-    def printResults(self):
-        print "-"*50
+    def print_results(self):
+        print "-" * 50
         for i, r in enumerate(self.results):
-            # print i, r["Title"], "(" + r["Url"] + ")"
-            # print " ", r["Description"].encode('ascii','ignore')
-            # print "-"*50
-            print config.print_str.format(i, r['Title'], r['Url'], r["Description"].encode('ascii','ignore'))
+            print config.print_str.format(i, r['Title'], r['Url'], r["Description"].encode('ascii', 'ignore'))
+
     def start(self):
-        self.results = self.getResults()
-        self.printResults()
+        self.results = self.get_results()
+        self.print_results()
         print "Enter which all results are relevant (comma separated):",
         relevant_indices = raw_input()
         self.evaluate(relevant_indices)
 
-    @staticmethod
     def evaluate(self, response):
-        self.selectedIDs = map(lambda x: int(x.strip()), \
+        """
+        
+        :param self:
+        :param response:
+        :return:
+        """
+        self.selectedIDs = map(lambda x: int(x.strip()),
                                response.split(','))
-        if len(self.selectedIDs)/10.0 >= self.precision: return
-        self.updateQuery()
+        if len(self.selectedIDs) / 10.0 >= self.precision: return
+        self.update_query()
 
-    def updateQuery():
+    @staticmethod
+    def update_query():
         # fix query and restart start
+        """
+
+
+        """
         print "Continue"
 
 
